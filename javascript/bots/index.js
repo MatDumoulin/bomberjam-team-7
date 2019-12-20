f383af73a41cd425d0f612ed20740f82e79baaf6
martin bouchard <martin.bouchard@gsoft-group.com>
2019-12-20 14:50:36 -0500

    final martin

--- javascript/bots/index.js
+++ javascript/bots/index.js
@@ -1,9 +1,10 @@
 const baseModel = require("./base-model");
+const baseModel2 = require("./base-model-2-deeper");
 
 const { RandomBot } = require("./randomBot");
 
 module.exports = {
     RandomBot,
     bot: baseModel,
-    botsToCompare: [baseModel, baseModel, baseModel, baseModel]
+    botsToCompare: [baseModel, baseModel2, baseModel2, baseModel]
 }
